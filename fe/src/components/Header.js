195a1fe9e8a6557758db9d90f4f3e81c8414d52d
fiazbilal <fiaz.bilal1@gmail.com>
2024-10-03 18:56:29 +0500

    feat: added header reuseable  component

---
+++ fe/src/components/Header.js
@@ -0,0 +1,23 @@
+// src/components/Header.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          {/* Add more links as needed */}
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
